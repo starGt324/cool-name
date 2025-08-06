cb6bcfb8ad15f4fd12a4e7ce9cc121db1b0afd5d
starGt324 <stargm324@gmail.com>
2025-08-06 21:44:55 +0100

    stable ver 1 is done.

---
+++ names.py
@@ -0,0 +1,22 @@
+import secrets
+import string
+#get names from file
+
+with open("cool_name.txt","r+") as f :
+    global strlist
+    strlist=[]
+    for line in f.readlines():
+        str_filter=str(line.replace("\n",""))
+        strlist.append(str_filter)
+
+
+ 
+ 
+int_ran=secrets.randbelow(999)
+
+#print(int_ran)
+#gen cool name
+
+ran_names=secrets.choice(strlist)
+ran_names2=secrets.choice(strlist)
+print(ran_names,ran_names2,int_ran)
